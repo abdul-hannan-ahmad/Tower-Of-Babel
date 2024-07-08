5b0172bdddc0ec430c4d6007e3269581ef4cc47f
abdul-hannan-ahmad <157882915+abdul-hannan-ahmad@users.noreply.github.com>
2024-07-08 12:12:36 +0500

    Delete {{PLATFORMER GAME HERE}} directory

--- {{PLATFORMER GAME HERE}}/code/player.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody2D
-
-@export var jump_force := 700.0
-
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		linear_velocity.y -= jump_force
